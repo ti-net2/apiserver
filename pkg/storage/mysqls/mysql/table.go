f3712375c4ccea7b257e3d9cbf8f7f841c8fb699
seanchann <seanchann.zhou@gmail.com>
2019-02-21 10:38:33 +0800

    fix table compiler error

--- pkg/storage/mysqls/mysql/table.go
+++ pkg/storage/mysqls/mysql/table.go
@@ -1,14 +1,14 @@
-// /*
+/*
 
-// Copyright 2018 This Project Authors.
+Copyright 2018 This Project Authors.
 
-// Author:  seanchann <seanchann@foxmail.com>
+Author:  seanchann <seanchann@foxmail.com>
 
-// See docs/ for more information about the  project.
+See docs/ for more information about the  project.
 
-// */
+*/
 
-// package mysql
+ package mysql
 
 // import (
 // 	"context"
